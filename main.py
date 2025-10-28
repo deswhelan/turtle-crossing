5085a6f87bc2113810a10e0f058b167e7d14bbc4
Des Whelan <wheland6@tcd.ie>
2025-10-28 09:48:38 +0000

    initial commit, including todos

---
+++ main.py
@@ -0,0 +1,13 @@
+# TODO: Configure screen
+    # must be 600 X 600
+    # turn off tracer and use update to refresh every 0.1s
+
+# TODO: Create Turtle class
+    # move forward when Up key is pressed
+    # can ONLY move forward; not back, left or right
+    # when turtle reaches top edge of screen, it moves back to original position and the player "levels up" (i.e. car speed increases)
+
+# TODO: Create Car class
+    # cars are randomly generated along y-axis and move from right edge to left edge of screen
+    # randomise car colour
+    # if car collides with turtle, game is over and everything stops

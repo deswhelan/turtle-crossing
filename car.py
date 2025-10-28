640c14de7596cbb4fc6dcd37812600fbbc732b81
Des Whelan <wheland6@tcd.ie>
2025-10-28 11:59:26 +0000

    move 10 cars across screen

--- car.py
+++ car.py
@@ -4,11 +4,8 @@ from turtle import Turtle
 #TODO: stretch - include a black car by giving it a white border/outline
 COLOURS = ["floral white", "gray", "silver", "royal blue", "firebrick"]
 SHAPE = "square"
-STARTING_CAR_SPEED = 0.1
 
-# TODO: Create Car class
-    # cars are randomly generated along y-axis and move from right edge to left edge of screen
-    # if car collides with turtle, game is over and everything stops
+# TODO:  if car collides with turtle, game is over and everything stops
 
 class Car(Turtle):
     def __init__(self, road):
@@ -21,7 +18,6 @@ class Car(Turtle):
         self.penup()
         self.goto(self.get_random_starting_position())
         self.setheading(180)
-        self.move(STARTING_CAR_SPEED)
         # TODO: stretch - implement start/finishing positions based on road width
         # self.starting_position = (0, -(self.road.window_height() / 2) + 20)
         # self.finishing_position = (0, self.road.window_height() / 2)
@@ -29,10 +25,9 @@ class Car(Turtle):
     @staticmethod
     def get_random_starting_position():
         """returns a tuple representing a random starting position along the car starting line"""
-        return 310, random.randint(-260, 260)
+        return 320, random.randint(-230, 230)
 
     def move(self, speed):
         """moves the car across the screen right to left at a given speed"""
-        while self.xcor() >= -320:
-            self.fd(speed)
-            self.road.update()
+        self.fd(speed)
+        self.road.update()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from car import Car
 from frog import Frog
 from turtle import Screen
 
+STARTING_CAR_SPEED = 1
+
 # TODO: stretch - move screen/road to its own class
 road = Screen()
 road.bgcolor("black")
@@ -14,6 +16,20 @@ road.tracer(0)
 frog = Frog(road)
 road.onkeypress(frog.move, "Up")
 
-car = Car(road)
+game_is_on = True
+cars = [Car(road) for i in range(10)]
+car_speed = STARTING_CAR_SPEED
+
+while game_is_on:
+
+    # TODO: cars are randomly generated along y-axis and move from right edge to left edge of screen
+    cars[0].move(car_speed)
+
+    for idx, car in enumerate(cars):
+        if idx == 0:
+            pass
+        else:
+            car.goto(cars[idx - 1].xcor() + 30, cars[idx].ycor())
+            road.update()
 
 road.exitonclick()
